cd0fe9b2eef83ffa19693489e45d911fcc8f0b99
meher-guesmi <76886909+meher-guesmi@users.noreply.github.com>
2024-01-16 11:46:53 +0100

    read input flask

--- app/routes/main_routes.py
+++ app/routes/main_routes.py
@@ -1,5 +1,18 @@
+from flask import jsonify, request
 from app import app
 
-@app.route("/")
-def hello_world():
-    return "Hello, World!"
+@app.route('/api/calculate_price_difference', methods=['POST'])
+def api_calculate_price_difference():
+    try:
+        request_data = request.get_json()
+
+        calculation_type = request_data.get('calculation_type')
+        percent_threshold = request_data.get('percent_threshold')
+        euro_threshold = request_data.get('euro_threshold')
+
+        results = [calculation_type, percent_threshold, euro_threshold]
+
+        return jsonify(results)
+
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
